25819471de9f71727697737bc70ab92fd18e23d4
Ashish Saha <49359207+Ashish357@users.noreply.github.com>
2023-09-17 11:25:17 +0530

    Update Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Shimmer = () => {
   return (
-    <div data-testid='shimmer' className="flex flex-wrap gap-7 justify-center m-28">
+    <div data-testid='shimmer' className="flex flex-wrap gap-7 justify-center mx-28">
       {Array(10)
         .fill("")
         .map((e, index) => (
@@ -12,7 +12,7 @@ const Shimmer = () => {
           //   <div className='bg-gray-50 m-2 h-6'></div>
           //   <div className='bg-gray-50 m-2 h-5'></div>
           // </div>
-          <div className=" animate-pulse w-72 h-96" key={index}>
+          <div className=" animate-pulse w-56 h-80" key={index}>
             <div className='bg-gray-400 rounded-lg m-2 h-2/4'></div>
             <div className='bg-gray-400 m-2 h-7 '></div>
             <div className='bg-gray-400 m-2 h-6 w-3/4'></div>
